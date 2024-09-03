8a211d53b5d3ea84f3a08c249213eb261e654204
a-sync <a-sync@devs.space>
2024-09-03 17:42:38 +0200

    Multi-page layout #60

--- src/app/components/Resume/ResumePDF/common/SuppressResumePDFErrorMessage.tsx
+++ src/app/components/Resume/ResumePDF/common/SuppressResumePDFErrorMessage.tsx
@@ -8,7 +8,8 @@ if (typeof window !== "undefined" && window.location.hostname === "localhost") {
   const consoleError = console.error;
   const SUPPRESSED_WARNINGS = ["DOCUMENT", "PAGE", "TEXT", "VIEW"];
   console.error = function filterWarnings(msg, ...args) {
-    if (!SUPPRESSED_WARNINGS.some((entry) => args[0]?.includes(entry))) {
+    const wrapFalse = Boolean(args[0] === "false" && args[1] === "wrap");
+    if (!SUPPRESSED_WARNINGS.some((entry) => args[0]?.includes(entry)) && !wrapFalse) {
       consoleError(msg, ...args);
     }
   };
