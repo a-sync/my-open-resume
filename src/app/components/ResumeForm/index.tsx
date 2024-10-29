71e854a4faf5016dd74dfdd9e634f9c29bce70c5
a-sync <a-sync@devs.space>
2024-10-29 17:04:55 +0100

    fix: jumping scrollbar #123

--- src/app/components/ResumeForm/index.tsx
+++ src/app/components/ResumeForm/index.tsx
@@ -34,7 +34,7 @@ export const ResumeForm = () => {
   return (
     <div
       className={cx(
-        "flex justify-center scrollbar scrollbar-track-gray-100 scrollbar-w-3 md:h-[calc(100vh-var(--top-nav-bar-height))] md:justify-end md:overflow-y-scroll",
+        "flex justify-center scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-gray-100 md:h-[calc(100vh-var(--top-nav-bar-height))] md:justify-end md:overflow-y-scroll",
         isHover && "scrollbar-thumb-gray-200"
       )}
       onMouseOver={() => setIsHover(true)}
